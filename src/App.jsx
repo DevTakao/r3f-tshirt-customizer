4f02f1e976b4e16e4b24ecbb6e47031c7d2f86d8
Taka <taka@visibleone.com>
2023-11-06 21:53:14 +0630

    feat: setting up layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import "./App.css";
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <p className="font-bold">hi</p>
-    </>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
 
